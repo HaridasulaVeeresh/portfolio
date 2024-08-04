bc0f3f1149e9acc86ed656675b8e77c729792d3f
vtu20334 <vtu20334@veltech.edu.in>
2024-08-04 22:36:08 +0530

    Hi

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function toggle (){
+    const menu =document.querySelector(".menu-links");
+    const icons =document.querySelector(".menu-items");
+    menu.classList.toggle("open")
+    icons.classList.toggle("open")
+}
